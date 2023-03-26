1a87249c83b0665ab62f2c8d0033d678f4da49c8
Chris Deck <deckchris95@gmail.com>
2023-03-26 18:24:16 -0400

    starting dev

--- App.tsx
+++ App.tsx
@@ -1,18 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { useState } from 'react';
+import { Text, TextInput, View, Image } from 'react-native';
+import styles from './styles';
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+import notFound from './assets/404.png';
+
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  // const [loaded] = useFonts({
+  //   Montserrat: require('./assets/fonts/BebasNeue-Regular.ttf'),
+  // });
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.searchBox}>
+        <FontAwesomeIcon icon={faLocationDot} />
+          <TextInput
+            style={styles.locationInput}
+            placeholder="Enter your location"
+            onChangeText={newText => setText(newText)}
+            defaultValue={text}/>
+          <FontAwesomeIcon icon={faSearch} style={styles.search}/>
+      </View>
+      <View style={styles.notFound}>
+        <Image source={notFound}/>
+        <Text style={{ fontSize: 27 }}>Oops! Invalid location. Please try again.</Text>
+      </View>
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function useFonts(arg0: { Montserrat: any; }): [any] {
+  throw new Error('Function not implemented.');
+}
+
---
+++ styles.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    searchBox: {
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: '#fff',
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: 50,
+        left: 25,
+        height: 100
+    },
+    locationInput: {
+        padding: 5,
+        margin: 10,
+        height: 40, 
+        width: "80%",
+        borderRadius: 7,
+        borderColor: 'gray',
+        borderWidth: 1,
+        backgroundColor: 'transparent',
+    },
+    search: {
+        
+        borderRadius: 5,
+        height: 100
+    },
+    notFound: {
+        width: "100%",
+        height: "50%",
+        justifyContent: "center",
+        textAlign: "center",
+        alignItems: "center"
+    }, 
+    notFoundText: {
+        textAlign: "center",
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+  });
+
+export default styles;
