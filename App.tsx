1f907b7407b63debcfa7351eae5d02e832d88e08
Chris Deck <deckchris95@gmail.com>
2023-03-27 12:23:19 -0400

    loaded bebas neue

--- App.tsx
+++ App.tsx
@@ -1,22 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Text, TextInput, View, Image } from 'react-native';
 import styles from './styles';
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
 import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
 import notFound from './assets/404.png';
+import { useFonts, BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue';
+import AppLoading from "expo-app-loading";
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 
 export default function App() {
   const [text, setText] = useState('');
+  let [fontsLoaded] = useFonts({
+    BebasNeue_400Regular,
+  });
 
-  // const [loaded] = useFonts({
-  //   Montserrat: require('./assets/fonts/BebasNeue-Regular.ttf'),
-  // });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
-  // if (!loaded) {
-  //   return null;
-  // }
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
@@ -31,14 +40,12 @@ export default function App() {
       </View>
       <View style={styles.notFound}>
         <Image source={notFound}/>
-        <Text style={{ fontSize: 27 }}>Oops! Invalid location. Please try again.</Text>
+        <Text style={{ fontFamily: 'BebasNeue_400Regular', fontSize: 27 }}>Oops! Invalid location. Please try again.</Text>
       </View>
       
     </View>
   );
 }
 
-function useFonts(arg0: { Montserrat: any; }): [any] {
-  throw new Error('Function not implemented.');
-}
+
 
